5ef3f0f65e7e85c7a5f9743b1f4a669a1ec9788b
Vladislav125 <112899378+Vladislav125@users.noreply.github.com>
2023-11-23 11:00:10 +0300

    Mistakes corrected

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,5 +4,5 @@ fun main() {
         .map { it.toString().toInt()}.toMutableList()
         .mapIndexed() { i, value -> if ((i + 1) % 2 == 0) value * 2 else value }
         .map { if (it > 9) it % 10 + it / 10 else it }.sum()
-    if (sum % 10 == 0) print("Корректный номер карты") else print("Неорректный номер карты")
+    if (sum % 10 == 0) print("Корректный номер карты") else print("Некорректный номер карты")
 }
